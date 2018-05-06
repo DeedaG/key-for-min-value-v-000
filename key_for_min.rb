fffea8f493ddc75642076b5483d5386e2d62462d
Christia T <christiatidwell@bellsouth.net>
2018-05-06 20:34:12 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.sort.shift(key)
+  if name_hash.sort.shift
     else
       nil
   end
