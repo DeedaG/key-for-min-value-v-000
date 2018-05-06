99eb1ff78c95fc6f8fbac2af13b8a1a70ad86d33
Christia T <christiatidwell@bellsouth.net>
2018-05-06 19:45:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash, i)
+def key_for_min_value(name_hash, index)
   name_hash.each do |key, value|
     key.keep_if(value[index] < value[index] + 1])
   end
